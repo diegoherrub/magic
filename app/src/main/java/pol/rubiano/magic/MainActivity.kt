fc7d97f9695543f524a66093a86c24d62d59a3c6
Diego Hernández Rubiano <diego.herrub@educa.jcyl.es>
2024-11-21 11:42:56 +0100

    Implemented menu bottom and random card fragment

--- app/src/main/java/pol/rubiano/magic/MainActivity.kt
+++ app/src/main/java/pol/rubiano/magic/MainActivity.kt
@@ -1,58 +1,24 @@
 package pol.rubiano.magic
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import pol.rubiano.magic.features.domain.random.domain.Card
-import pol.rubiano.magic.features.domain.random.presentation.RandomCardFactory
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
-
-    private val randomCardFactory: RandomCardFactory = RandomCardFactory()
-    private val viewModel = randomCardFactory.buildRandomCardViewModel()
+class MainActivity: AppCompatActivity() {
 
     override  fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val randomCard = viewModel.viewCreated()
-        viewModel.itemSelected()
-        bindData(randomCard)
+        setupView()
     }
 
-    private fun bindData(randomCard: Card) {
-        findViewById<TextView>(R.id.id).text = randomCard.id
-        findViewById<TextView>(R.id.name).text = randomCard.name
-        findViewById<TextView>(R.id.mana_cost).text = randomCard.manaCost
-        findViewById<TextView>(R.id.type_line).text = randomCard.typeLine
-        findViewById<TextView>(R.id.oracle_text).text = randomCard.oracleText
-        findViewById<TextView>(R.id.flavor_text).text = randomCard.flavorText
-        findViewById<TextView>(R.id.set_name).text = randomCard.setName
-        findViewById<TextView>(R.id.released_at).text = randomCard.releaseAt
-        findViewById<TextView>(R.id.rarity).text = randomCard.rarity
-        findViewById<TextView>(R.id.artist).text = randomCard.artist
-    }
+    private fun setupView() {
+        val navHosFragment = supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+        val navController = navHosFragment.navController
 
-    override fun onStart() {
-        super.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(navController)
     }
 }
 
---
+++ app/src/main/java/pol/rubiano/magic/features/domain/random/presentation/RandomCardFragment.kt
@@ -0,0 +1,6 @@
+package pol.rubiano.magic.features.domain.random.presentation
+
+import androidx.fragment.app.Fragment
+
+class RandomCardFragment: Fragment() {
+}
