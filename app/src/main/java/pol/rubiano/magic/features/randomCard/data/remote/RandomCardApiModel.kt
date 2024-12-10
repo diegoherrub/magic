5fe9f54271df101f9cdb989299502e387818b574
Diego Rubiano <diego@pigames.es>
2024-12-10 14:05:00 +0100

    241210_wip kdocs

--- app/src/main/java/pol/rubiano/magic/features/randomCard/data/remote/RandomCardApiModel.kt
+++ app/src/main/java/pol/rubiano/magic/features/randomCard/data/remote/RandomCardApiModel.kt
@@ -2,6 +2,23 @@ package pol.rubiano.magic.features.randomCard.data.remote
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * Data class representing a random card API model.
+ *
+ * This class is used to parse the JSON response from the Scryfall API.
+ *
+ * @property id The unique identifier of the card.
+ * @property name The name of the card.
+ * @property manaCost The mana cost of the card.
+ * @property typeLine The type line of the card.
+ * @property oracleText The oracle text of the card.
+ * @property flavorText The flavor text of the card.
+ * @property imageUris The image URIs of the card.
+ * @property setName The name of the set the card belongs to.
+ * @property releasedAt The release date of the card.
+ * @property rarity The rarity of the card.
+ * @property artist The artist of the card.
+ */
 data class RandomCardApiModel(
     @SerializedName("id") val id: String,
     @SerializedName("name") val name: String,
@@ -16,6 +33,11 @@ data class RandomCardApiModel(
     @SerializedName("artist") val artist: String,
 )
 
+/**
+ * Data class representing the image URIs of a card.
+ *
+ * @property borderCrop The URL of the border crop image.
+ */
 data class ImageUris (
     @SerializedName("border_crop") val borderCrop: String
 )
